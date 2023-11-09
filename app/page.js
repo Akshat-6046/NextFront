3ea2816002e59c4528915b2f443cef3f71524e10
AkshatSingh-6046 <akshat.singh@cogoport.com>
2023-11-09 20:30:21 +0530

    commit first

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+export default function Home() {
+  return (
+    <main>
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="flex justify-between items-center font-white">
+          Hello
+        </div>
+      </div>
+    </main>
+  );
+}
